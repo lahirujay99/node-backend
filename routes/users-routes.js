4b0ff70eece7ba3f5b1ca937202b75def06738c4
Lahiru Jay <it21187100@my.sliit.lk>
2023-02-02 02:01:51 +0530

    added validation logic to the relavant functions

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check } = require('express-validator');
 
 const userControllers = require('../controllers/users-controller');
 
@@ -6,7 +7,15 @@ const router = express.Router();
 
 router.get('/', userControllers.getAllUsers);
 
-router.post('/signup', userControllers.signup);
+router.post(
+  '/signup',
+  [
+    check('name').not().isEmpty(),
+    check('email').normalizeEmail().isEmail(), //normalize email = Test@t.com => test@t.com
+    check('password').isLength({ min: 6 }),
+  ],
+  userControllers.signup
+);
 
 router.post('/login', userControllers.login);
 
